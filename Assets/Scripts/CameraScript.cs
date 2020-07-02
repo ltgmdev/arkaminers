db55aed69449e859cdea9e93d2921da35fd7c863
ltgmdev <67628523+ltgmdev@users.noreply.github.com>
2020-07-01 21:40:50 -0300

    1jul

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public float targetRatio = 9f / 16f; //The aspect ratio of your game
+    void Start()
+    {
+        Camera cam = GetComponent<Camera>();
+        cam.aspect = targetRatio;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
